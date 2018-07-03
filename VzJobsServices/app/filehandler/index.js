2f0b8ecd6864cff752abce7dd88af094c6dae1a1
Praveen Kumar S <praveenkumar.subramaniyan@gmail.com>
2018-07-03 09:12:22 +0530

    updating file storage resource

--- VzJobsServices/app/filehandler/index.js
+++ VzJobsServices/app/filehandler/index.js
@@ -2,7 +2,15 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var logger = require('morgan');
 var multer = require('multer');
-var upload = multer({ dest: './uploads/'});
+var storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+      cb(null, '/tmp/my-uploads')
+    },
+    filename: function (req, file, cb) {
+      cb(null, file.fieldname + '-' + Date.now())
+    }
+  });
+var upload = multer({ storage: storage });
 
 var app = express();
 app.use(bodyParser.json());
